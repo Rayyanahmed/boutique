1d7ec150299f286983cceb32addf3694ec9c457d
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 15:21:15 -0500

    notes

--- notes.php
+++ notes.php
@@ -11,5 +11,6 @@
 
 
 // Added Men, Women, Girls, Boys in our categories table. These will be our top level menu items
+// changed parent column in the category table to parent_id, this will correlate to the ID of your parent category
 
 ?>
