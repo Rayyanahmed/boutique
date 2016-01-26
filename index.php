28303f9e0e450c5733765a526bae804a0b6bc868
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 10:40:30 -0500

    added cols

--- index.php
+++ index.php
@@ -146,6 +146,8 @@
 				give it cols -->
 				<div class="container-fluid">
 					<div class="row">
+						<div class="col-sm-6"></div>
+						<div class="col-sm-6"></div>
 					</div>
 				</div>
 			</div>
