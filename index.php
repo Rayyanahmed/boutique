3efd2a34f8b8614b65ec7ac24bd39c66671bd793
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 11:03:06 -0500

    this div is needed for the background to be solid

--- index.php
+++ index.php
@@ -134,38 +134,40 @@
 
 	<div class="modal fade details-1" id="details-1" tabindex="-1" role="dialog" aria-label="details-1" aria-hidden="true">
 		<div class="modal-dialog modal-lg">
-			<div class="modal-header">
-				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-				<!-- all going to be dynamic later -->
-				<h4 class="modal-title" class="text-center">Levis Jeans</h4>
-			</div>
-			<div class="modal-body">
-				<!-- we are going to put container fluid inside the modal body so we can
-				give it cols -->
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-sm-6">
-							<!-- to wrap image around -->
-							<div class="center-block">
-								<img src="images/products/men4.png" alt="Levis Jeans" class="details img-responsive">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<!-- all going to be dynamic later -->
+					<h4 class="modal-title" class="text-center">Levis Jeans</h4>
+				</div>
+				<div class="modal-body">
+					<!-- we are going to put container fluid inside the modal body so we can
+					give it cols -->
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-6">
+								<!-- to wrap image around -->
+								<div class="center-block">
+									<img src="images/products/men4.png" alt="Levis Jeans" class="details img-responsive">
+								</div>
 							</div>
-						</div>
-						<div class="col-sm-6">
-							<h4>Details</h4>
-							<p>These jeans are amazing, they are straight-leg, fit great
-								and look sexy. Get a pair while they last.</p>
-							<hr>
-							<p>Price: $34.99</p>
-							<p>Brand: Levis</p>
+							<div class="col-sm-6">
+								<h4>Details</h4>
+								<p>These jeans are amazing, they are straight-leg, fit great
+									and look sexy. Get a pair while they last.</p>
+								<hr>
+								<p>Price: $34.99</p>
+								<p>Brand: Levis</p>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="modal-footer">
-				<button class="btn btn-default" data-dismiss="modal">Close</button>
-				<button class="btn btn-warning" type="submit" class="glyphicon glyphicon-shopping-cart"><span>Add to Cart</span></button>
+				<div class="modal-footer">
+					<button class="btn btn-default" data-dismiss="modal">Close</button>
+					<button class="btn btn-warning" type="submit" class="glyphicon glyphicon-shopping-cart"><span>Add to Cart</span></button>
+				</div>
 			</div>
 		</div>
 	</div>
