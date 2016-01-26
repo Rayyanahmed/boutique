a1104b05e37fc1ea83e67286ece0acbabbea774b
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 23:52:01 -0500

    finished navbar

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
 	<title>Shaunta's Boutique</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script type="text/javascript src=js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top">
@@ -15,6 +15,12 @@
 			<ul class="nav navbar-nav">
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Shirts</a></li>
+						<li><a href="#">Pants</a></li>
+						<li><a href="#">Shoes</a></li>
+						<li><a href="#">Accessories</a></li>
+					</ul>
 				</li>
 			</ul>
 		</div>
