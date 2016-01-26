8b36723fd201a220bb3c4c7b47175d62a118db24
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 23:05:52 -0500

    works on other devices

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Shaunta's Boutique</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 </head>
 <body>
 </body>
