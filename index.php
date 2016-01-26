29609933c18f26258093e909ce139b89f6301aa5
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 01:45:47 -0500

    working on col

--- index.php
+++ index.php
@@ -47,6 +47,10 @@
 				<h2 class="text-center">Featured Products</h2>
 				<div class="col-md-3">
 					<h4>Levis Jeans</h4>
+					<img src="images/products/men4.png" alt="Levis Jeans" />
+					<!-- All these classes so we can target later -->
+					<p class="list-price text-danger">List Price <s>$54.99</s></p>
+					<p class="price">Our Price: 19.99</p>
 				</div>
 			</div>
 		</div>
