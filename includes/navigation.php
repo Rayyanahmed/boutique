c01b991429a40a89321c5595ab42d0956a48dcb1
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 16:17:15 -0500

    added top level menu items dynamically

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,7 +4,7 @@ $sql = "SELECT * FROM categories WHERE parent_id = 0";
 
 $parent_query = mysqli_query($db, $sql);
 
-print_r($parent_query);
+// We dont need the IF statement this works as it is because we queried for categories WHERE the parent_id = 0
 
 ?>
 
@@ -14,9 +14,9 @@ print_r($parent_query);
 			<ul class="nav navbar-nav">
 				<?php while($row = mysqli_fetch_assoc($parent_query)) : ?>
 				<?php $parent_id = $row['parent_id']; ?>
-				<?php echo $parent_id; ?>
+		
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $row['category']; ?><span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
 						<li><a href="#">Shirts</a></li>
 						<li><a href="#">Pants</a></li>
