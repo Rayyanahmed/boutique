092656e8a5deba4636297b7efe6b84083a19a2f1
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 14:43:24 -0500

    notes

--- notes.php
+++ notes.php
@@ -3,4 +3,6 @@
 // Created a table with id, title, price, list_price, brand, categories (will have multiple), image (one image), description
 // For my next project I will implement multiple images
 
+// Added brand table: field - brand, varchar 255. 
+
 ?>
