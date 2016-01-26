7509a6220fe7198a1d6646283b57d2eccb1f04e9
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 10:36:17 -0500

    working on modal

--- index.php
+++ index.php
@@ -133,7 +133,15 @@
 	<!-- Details Modal -->
 
 	<div class="modal fade details-1" id="details-1" tabindex="-1" role="dialog" aria-label="details-1" aria-hidden="true">
-		
+		<div class="modal-dialog modal-lg">
+			<div class="modal-header">
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<!-- all going to be dynamic later -->
+				<h4 class="modal-title" class="text-center">Levis Jeans</h4>
+			</div>
+		</div>
 	</div>
 
 	<script>
