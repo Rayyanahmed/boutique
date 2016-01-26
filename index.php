99e28a97acc954c4b0d84df5c8a5e02db8ec64aa
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-25 23:04:22 -0500

    added css files to index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shaunta's Boutique</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+</body>
+</html>
