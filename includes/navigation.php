683dcaaa476afa4f431044b667203c506539d2ae
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 16:33:37 -0500

    figured it out, i rock, finished the entire subcategory menu

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,7 +13,7 @@ $parent_query = mysqli_query($db, $sql);
 			<ul class="nav navbar-nav">
 				<?php while($row = mysqli_fetch_assoc($parent_query)) : ?>
 				<?php 
-					$parent_id = $row['parent_id']; 
+					$parent_id = $row['id']; 
 					$sql2 = "SELECT * FROM categories WHERE parent_id = '" . $parent_id . "' ";
 					$child_query = mysqli_query($db, $sql2);
 				?>
