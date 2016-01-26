31b1d87dbce12ff01cf075305167617d96c6cd48
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 16:26:20 -0500

    made a second query, will have to make a second while loop in order to place in the subcategories

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,7 +1,6 @@
 <?php 
 
 $sql = "SELECT * FROM categories WHERE parent_id = 0";
-
 $parent_query = mysqli_query($db, $sql);
 
 // We dont need the IF statement this works as it is because we queried for categories WHERE the parent_id = 0
@@ -13,14 +12,16 @@ $parent_query = mysqli_query($db, $sql);
 			<a href="index.php" class="navbar-brand">Shaunta's Boutique</a>
 			<ul class="nav navbar-nav">
 				<?php while($row = mysqli_fetch_assoc($parent_query)) : ?>
-				<?php $parent_id = $row['parent_id']; 
-					  $sql2 = "SELECT * FROM categories WHERE parent_id == '$parent_id'"
+				<?php 
+					$parent_id = $row['parent_id']; 
+					$sql2 = "SELECT * FROM categories WHERE parent_id == '$parent_id'"
+					$child_query = mysqli_query($db, $sql2);
 				?>
 		
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $row['category']; ?><span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="#">Shirts</a></li>
+						<li><a href="#"><?php if() ?></a></li>
 						<li><a href="#">Pants</a></li>
 						<li><a href="#">Shoes</a></li>
 						<li><a href="#">Accessories</a></li>
