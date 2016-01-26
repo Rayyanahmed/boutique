97a265222c4c5ea951aa0ec11911a791e6cb665d
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 16:32:16 -0500

    not working

--- includes/navigation.php
+++ includes/navigation.php
@@ -14,17 +14,16 @@ $parent_query = mysqli_query($db, $sql);
 				<?php while($row = mysqli_fetch_assoc($parent_query)) : ?>
 				<?php 
 					$parent_id = $row['parent_id']; 
-					$sql2 = "SELECT * FROM categories WHERE parent_id == '$parent_id'"
+					$sql2 = "SELECT * FROM categories WHERE parent_id = '" . $parent_id . "' ";
 					$child_query = mysqli_query($db, $sql2);
 				?>
 		
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $row['category']; ?><span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="#"><?php if() ?></a></li>
-						<li><a href="#">Pants</a></li>
-						<li><a href="#">Shoes</a></li>
-						<li><a href="#">Accessories</a></li>
+						<?php while($row2 = mysqli_fetch_assoc($child_query)) : ?>
+						<li><a href="#"><?php echo $row2['category']; ?></a></li>
+					<?php endwhile; ?>
 					</ul>
 				</li>
 				<?php endwhile; ?>
