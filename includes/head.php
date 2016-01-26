932bc8779f0d88223ff529e62f3d1d683586ee3a
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-26 14:51:01 -0500

    seperated the header out

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shaunta's Boutique</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Shaunta's Boutique</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-</head>
-<body>
+<?php include('includes/head.php') ?>
 	<!-- Top Nav Bar -->
 	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
