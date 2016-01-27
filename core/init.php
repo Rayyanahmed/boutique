45912e33c493d2017bf8ac0af6c1929be819dda6
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-27 13:44:55 -0500

    added config file

--- core/init.php
+++ core/init.php
@@ -9,7 +9,7 @@ if(mysqli_connect_errno($db)) {
 
 // We defined the config file as a relative file instead of an absolute file, which is fine for
 // being required in the admin folder but breaks our main site
-require_once('../config.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/boutique/config.php');
 require_once(BASEURL . 'helpers/helpers.php');
 
 
