34d6fa71b38ec4e5b4598e21ba929a6774b33601
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-27 10:49:50 -0500

    working on table

--- admin/brands.php
+++ admin/brands.php
@@ -2,4 +2,16 @@
 <?php require_once('includes/head.php'); ?>
 <?php require_once('includes/navigation.php'); ?>
 Brands
+<table class="table table-bordered table-striped">
+	<head>
+		<th></th><th>Brand</th><th></th>
+	</head>
+	<tbody>
+		<tr>
+			<td></td>
+			<td>Levis</td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
 <?php require_once('includes/footer.php'); ?>
