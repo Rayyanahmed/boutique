cf1d176cde40229d193929d6f252415f4147a8da
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-27 10:28:30 -0500

    working on brands page for admin

---
+++ admin/brands.php
@@ -0,0 +1,5 @@
+<?php require_once('../core/init.php'); ?>
+<?php require_once('includes/head.php'); ?>
+<?php require_once('includes/navigation.php'); ?>
+Brands
+<?php require_once('includes/footer.php'); ?>
