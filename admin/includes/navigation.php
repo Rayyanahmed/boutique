661973eb1acbf7bf88599971e2764660bbe9ceb6
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-27 10:31:38 -0500

    working on admin navigation

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
-			<a href="index.php" class="navbar-brand">Shaunta's Boutique</a>
+			<a href="/boutique/admin/index.php" class="navbar-brand">Shaunta's Boutique</a>
 			<ul class="nav navbar-nav">
 				<li><a href="brands.php">Brands</a></li>
 			</ul>
