5634d53e28339d53795e53606047e9a6c86e04fc
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-27 17:59:15 -0500

    added table header

--- admin/categories.php
+++ admin/categories.php
@@ -7,8 +7,14 @@ include('includes/navigation.php');
 <h2 class="text-center">Categories</h2>
 
 <div class="row">
-	<div class="col-md-6">Test</div>
-	<div class="col-md-6">Test</div>
+	<div class="col-md-6"></div>
+	<div class="col-md-6">
+		<table class="table table-border">
+			<head>
+				<th>Category</th><th>Parent</th><th></th>
+			</head>
+		</table>
+	</div>
 </div>
 
 <?php 
