4ae7cf4dc9097afde45e688af27d9f8c149555ba
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-28 16:30:38 -0500

    need this here so the first option value will be blank

--- admin/products.php
+++ admin/products.php
@@ -16,6 +16,7 @@ if (isset($_GET['add'])) {
 		<div class="form-group col-md-3">
 			<label for="brand">Brand*</label>
 			<select class="form-control" id="brand" name="brand">
+				<option value=""></option>
 				<?php while($brand = mysqli_fetch_assoc($brandQuery)): ?>
 					<option value="<?php echo $brand['id']; ?>"><?php echo $brand['brand']; ?></option>
 				<?php endwhile; ?>
