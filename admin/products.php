bb9a12f5322e872ac7141efd984293ddaa182507
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-28 12:13:43 -0500

    working on table

--- admin/products.php
+++ admin/products.php
@@ -4,6 +4,9 @@ include('includes/head.php');
 include('includes/navigation.php');
 ?>
 
+<h2 class="text-center">Products</h2><hr>
+<table class="table table-border table-condensed table-striped"></table>
+
 
 <?php
 include('includes/footer.php');
