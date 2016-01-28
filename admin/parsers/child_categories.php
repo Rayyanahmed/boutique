b3e5f47bfa2269a3d13fc1a4c2fce776e29bc0b5
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-28 17:55:06 -0500

    require database

--- admin/parsers/child_categories.php
+++ admin/parsers/child_categories.php
@@ -1,4 +1,5 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/boutque/core/init.php');
 // In our ajax request we are sending this parentID as data
 $parentID = (int)($_POST['parentID']);
 $childQuery = mysqli_query($db, "SELECT * FROM categories WHERE parent_id = '$parentID' ORDER BY category");
