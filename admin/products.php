f6d0a03843fa5cfeca235d207ea193d3160782db
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-29 10:43:00 -0500

    added modal class to make larger

--- admin/products.php
+++ admin/products.php
@@ -68,7 +68,7 @@ if (isset($_GET['add'])) {
 
 	<!-- Modal -->
 <div class="modal fade" id="sizesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
