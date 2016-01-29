1f12750b5786af63d217568753da370d23746697
Rayyan Ahmed <rayyan.z.ahmed@gmail.com>
2016-01-29 16:18:48 -0500

    finally fixed the bug

--- admin/products.php
+++ admin/products.php
@@ -84,11 +84,11 @@ if (isset($_GET['add'])) {
 			</select>
 		</div>
 		<div class="form-group col-md-3">
-			<label for="parent">Parent Category*:</label>
+			<label for="parent">Parent Category:*</label>
 			<select class="form-control" id="parent" name="parent">
-				<option value="<?php echo ((isset($_POST['parent']) && $_POST['parent'] == '')?' selected':'');?>"></option>
+				<option value=""></option>
 				<?php while($parent = mysqli_fetch_assoc($parentQuery)): ?>
-					<option value="<?php $parent['id']; ?>"<?php echo ((isset($_POST['parent']) && $_POST['parent'] == $parent['id'])?' select':''); ?>><?php echo $parent['category']; ?></option>
+					<option value="<?php echo $parent['id']; ?>"><?php echo $parent['category']; ?></option>
 				<?php endwhile; ?>
 			</select>
 		</div>
